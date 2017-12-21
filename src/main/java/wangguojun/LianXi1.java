cbf11e7235033de87329e6bbb98e710648cfb7c4
wangguojun <wangguojun@DEEP-1707131816.lan>
2017-12-21 15:36:13 +0800

    LianXi1

---
+++ src/main/java/wangguojun/LianXi1.java
@@ -0,0 +1,36 @@
+/**
+ * Project Name:wangguojun
+ * File Name:LianXi1.java
+ * Package Name:wangguojun
+ * Date:2017年12月21日下午3:29:42
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+/**
+ * Project Name:wangguojun
+ * File Name:LianXi1.java
+ * Package Name:wangguojun
+ * Date:2017年12月21日下午3:29:42
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package wangguojun;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月21日 下午3:29:42 <br/>
+ * @author   wangguojun
+ * @version
+ * @see
+ */
+public class LianXi1 {
+
+    public static void main(String[] args) {
+
+        int[] ary = new int[] { 1, 2, 3, 4, };
+        for (int i = 0; i < ary.length; i++) {
+            System.out.println(ary[i]);
+        }
+
+    }
+
+}
+
