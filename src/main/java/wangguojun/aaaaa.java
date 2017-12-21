45e03ab3ea772c5e5cc168a9a9ec6e516f1d34cc
wangguojun <wangguojun@DEEP-1707131816.lan>
2017-12-21 14:20:43 +0800

    ssssss

---
+++ src/main/java/wangguojun/aaaaa.java
@@ -0,0 +1,33 @@
+/**
+ * Project Name:wangguojun
+ * File Name:aaaaa.java
+ * Package Name:wangguojun
+ * Date:2017年12月21日下午2:19:57
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+/**
+ * Project Name:wangguojun
+ * File Name:aaaaa.java
+ * Package Name:wangguojun
+ * Date:2017年12月21日下午2:19:57
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package wangguojun;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月21日 下午2:19:57 <br/>
+ * @author   wangguojun
+ * @version
+ * @see
+ */
+public class aaaaa {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
