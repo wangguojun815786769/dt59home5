36c279594d221e46a65bc20a9b40bf0d15178e0a
wangguojun <wangguojun@DEEP-1707131816>
2017-12-25 20:56:31 +0800

    home

---
+++ src/main/java/home20171225/Max.java
@@ -0,0 +1,44 @@
+/**
+ * Project Name:wangguojun
+ * File Name:Max.java
+ * Package Name:hw20171225
+ * Date:2017年12月25日下午5:03:01
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+/**
+ * Project Name:wangguojun
+ * File Name:Max.java
+ * Package Name:hw20171225
+ * Date:2017年12月25日下午5:03:01
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package home20171225;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月25日 下午5:03:01 <br/>
+ * @author   wangguojun
+ * @version
+ * @see
+ */
+public class Max {
+
+    public static void main(String[] args) {
+        Max max = new Max();
+        int num3 = 6;
+        int num4 = 9;
+        int num5 = max.max(num3, num4);
+        System.out.println(num5);
+    }
+
+    int max(int Max, int Max1) {
+        if (Max > Max1) {
+            return Max;
+        } else {
+            return Max1;
+        }
+
+    }
+
+}
+
